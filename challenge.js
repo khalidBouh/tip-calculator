b2c767142f8490a56cf28c194ea10aa57601de40
khalidBouh <khaalidm26@gmail.com>
2023-07-20 20:49:45 +0300

    Done Exercise

--- challenge.js
+++ challenge.js
@@ -1,2 +1,9 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+const bill = 300
+
+if(bill >= 50){
+    console.log( `The bill was ${bill} the tip was ${bill * 0.15 }, and the total value  ${bill + bill * 0.15 }`)
+} else if ( bill >= 300 ){
+    console.log( `The bill was ${bill} the tip was ${bill * 0.20 }, and the total value ${bill + bill * 0.20 }`)
+}
